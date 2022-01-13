e5602c5fc46308179e5b7e6c70b131544c1914ab
Rônei Társis <roneitarsissilvadaltro@gmail.com>
2022-01-13 16:58:46 -0300

    Errata frontend

--- frontend/src/compontes/Navbar/index.tsx
+++ frontend/src/compontes/Navbar/index.tsx
@@ -8,11 +8,12 @@ function Navbar() {
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https//github/roneitarsis" />
-                    <div className="dsmovie-contact-container">
-                        <GithubIcon />
-                        <p className="dsmovie-contact-link"> /devsuperior</p>
-                    </div>
+                    <a href="https//github/roneitarsis" >
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link"> /devsuperior</p>
+                        </div>
+                    </a>
                 </div>
             </nav>
         </header>
